131c846fbcf9ccc8793625c7c9cec1946d795e1b
Anudeep D <anudeepd@nbsotech.com>
2017-09-17 22:55:31 +0530

    first commit

--- common/directives/app.directives.js
+++ common/directives/app.directives.js
@@ -3,107 +3,84 @@
 
     angular
         .module('app')
-        .directive('gridTable', ['$timeout', function($timeout) {
+        .directive('gridTable', ['$timeout', 'orderByFilter', function($timeout, orderBy) {
             return {
                 restrict: 'EA',
                 templateUrl: 'common/directives/partials/gridTable.html',
                 replace: true,
+                scope: {
+                    sampleData: "=",
+                    columnData: "="
+                },
                 link: function(scope, iElement, iAttrs) {
-                    scope.sampleData = [];
-                    scope.sortColumn = 'type'; // set the default sort type
+                    scope.columns = angular.copy(scope.columnData)
+                    scope.sortColumn = 'topic'; // set the default sort type
                     scope.sortReverse = false;
-                    var dumpData = [{
-                        type: 'POS',
-                        extdesc: 'ABC VONS Store 1797 SAN DIEGO CAUS',
-                        description: 'POS Transaction VONS Store 1797 SAN DIEGO CAUS',
-                        amount: '-4.43',
-                        differed: false,
-                        date: '2016-02-23',
-                        action: 'Edit'
-                    }, {
-                        type: 'CC',
-                        extdesc: 'Amazon.com',
-                        description: 'online retailer Amazon.com',
-                        amount: '-76.99',
-                        differed: true,
-                        date: '2016-04-08',
-                        action: 'Edit'
-                    }, {
-                        type: 'POS',
-                        extdesc: 'shop #1',
-                        description: 'online retailer #1',
-                        amount: '-100',
-                        differed: false,
-                        date: '2016-05-12',
-                        action: 'Edit'
-                    }, {
-                        type: 'DDeb',
-                        extdesc: 'shop #2',
-                        description: 'retailer #2',
-                        amount: '-120',
-                        differed: false,
-                        date: '2016-06-17',
-                        action: 'Edit'
-                    }, {
-                        type: 'POS',
-                        extdesc: 'shop #3',
-                        description: 'online retailer #3',
-                        amount: '-130',
-                        differed: false,
-                        date: '2016-08-20',
-                        action: 'Edit'
-                    }, {
-                        type: 'CC',
-                        extdesc: 'shop #4',
-                        description: 'retailer #4',
-                        amount: '-320',
-                        differed: true,
-                        date: '2016-10-30',
-                        action: 'Edit'
-                    }, {
-                        type: 'POS',
-                        extdesc: 'shop #5',
-                        description: 'online retailer #5',
-                        amount: '-101',
-                        differed: false,
-                        date: '2016-11-01',
-                        action: 'Edit'
-                    }];
-
-                    var colObj = dumpData[0];
-                    scope.columns = [];
-                    var i = 0;
-                    for (var key in colObj) {
-                        scope.columns.push({
-                            "name": key,
-                            "position": i++,
-                        });
-                    }
-                    scope.sampleData = angular.copy(dumpData);
 
                     scope.sort = function(column) {
                         scope.sortColumn = column;
                         scope.sortReverse = !scope.sortReverse;
+                        scope.sampleData = orderBy(scope.sampleData, scope.sortColumn, scope.sortReverse);
                     }
-                    $timeout(function() {
-                        for (var i = 0; i <= scope.columns.length; i++) {
-                            $(".resizable" + i).resizable({
-                                alsoResize: ".connectedCol" + i
-                            });
-                            $(".connectedCol" + i).resizable();
+
+                    scope.save = function(row) {
+                        row.isEdit = false;
+                        if(row.tempData){
+                            delete row.tempData;
                         }
-                        $("#sortable").sortable({
-                            stop: function(e, ui) {
-                                var sortArray = $("#sortable").sortable("toArray");
-                                for (var i = 0; i < scope.columns.length; i++) {
-                                    scope.columns[i].position = sortArray[i];
-                                }
-                                scope.$apply(function() {
-                                });
+                    }
+
+                    scope.edit = function(row){
+                        row.tempData = angular.copy(row);
+                        row.isEdit = true;
+                    }
+
+                    scope.cancel = function(row, index) {
+                        
+                        if (row.id) {
+                            for(var i in row.tempData){
+                                row[i] = row.tempData[i]
                             }
+                            delete row.tempData;
+                            row.isEdit = false;
+                        } else {
+                            scope.sampleData.splice(index, 1);
+                        }
+                        
+                    }
+
+                    function init() {
+
+                        for (var i = 0; i < scope.sampleData.length; i++) {
+                            scope.sampleData[i].isEdit = false;
+                        }
+                        for (var i = 0; i < scope.columns.length; i++) {
+                            scope.columns[i].position = i;
+                        }
+
+                        scope.sampleData = orderBy(scope.sampleData, scope.sortColumn, scope.sortReverse);
+
+                        $timeout(function() {
+                            for (var i = 0; i <= scope.columns.length; i++) {
+                                $(".resizable" + i).resizable({
+                                    alsoResize: ".connectedCol" + i
+                                });
+                                $(".connectedCol" + i).resizable();
+                            }
+                            $("#sortable").sortable({
+                                stop: function(e, ui) {
+                                    var sortArray = $("#sortable").sortable("toArray");
+                                    for (var i = 0; i < scope.columns.length; i++) {
+                                        scope.columns[i].position = sortArray[i];
+                                    }
+                                    scope.$apply(function() {});
+                                }
+                            });
+                            $("#sortable").disableSelection();
                         });
-                        $("#sortable").disableSelection();
-                    });
+                    }
+
+                    init();
                 }
 
             };
--- home/home.js
+++ home/home.js
@@ -3,59 +3,126 @@
 
     angular
         .module('app')
-        .controller('HomeController', Controller);
+        .controller('HeaderController', HeaderController)
+        .controller('HomeController', HomeController);
 
-    function Controller($window) {
+    function HeaderController(){
         var vm = this;
-        var boringData = [{
-            type: 'POS',
-            extdesc: 'ABC VONS Store 1797 SAN DIEGO CAUS',
-            description: 'POS Transaction VONS Store 1797 SAN DIEGO CAUS',
-            amount: '-4.43',
-            differed: false,
-            date: '2016-02-23'
+        console.log("here");
+    }
+
+    function HomeController($window) {
+        var vm = this;
+
+        this.addRow = addRow;
+
+        function addRow() {
+            this.sampleData.unshift({
+                type: '',
+                extdesc: '',
+                description: '',
+                amount: '',
+                differed: '',
+                date: '',
+                isEdit: true
+
+            });
+            console.log(this.sampleData);
+        }
+        this.columnData = [{
+            "id": "topic",
+            "name": "Topic",
+            "inputType": "text"
         }, {
-            type: 'CC',
-            extdesc: 'Amazon.com',
-            description: 'online retailer Amazon.com',
-            amount: '-76.99',
-            differed: true,
-            date: '2016-04-08'
+            "id": "description",
+            "name": "Description",
+            "inputType": "textarea"
         }, {
-            type: 'POS',
-            extdesc: 'shop #1',
-            description: 'online retailer #1',
-            amount: '-100',
-            differed: false,
-            date: '2016-05-12'
+            "id": "speaker",
+            "name": "Speaker",
+            "inputType": "text"
         }, {
-            type: 'DDeb',
-            extdesc: 'shop #2',
-            description: 'retailer #2',
-            amount: '-120',
-            differed: false,
-            date: '2016-06-17'
+            "id": "attendies",
+            "name": "Attendies",
+            "inputType": "number"
         }, {
-            type: 'POS',
-            extdesc: 'shop #3',
-            description: 'online retailer #3',
-            amount: '-130',
-            differed: false,
-            date: '2016-08-20'
+            "id": "price",
+            "name": "Price",
+            "inputType": "text"
         }, {
-            type: 'CC',
-            extdesc: 'shop #4',
-            description: 'retailer #4',
-            amount: '-320',
-            differed: true,
-            date: '2016-10-30'
+            "id": "date",
+            "name": "Date",
+            "inputType": "date"
+        },{
+            "id": "webinar",
+            "name": "Webinar",
+            "inputType": "select",
+            "items": ["GoToWebinar", "Cisco WebEx", "ClickWebinar"]
+        },];
+
+        this.sampleData = [{
+            id: 1,
+            topic: 'Android Development',
+            description: 'Step by Step Android development for Beginners',
+            speaker: 'John',
+            attendies: 200,
+            price: 'Free',
+            date: '2017-10-23',
+            webinar: "GoToWebinar"
         }, {
-            type: 'POS',
-            extdesc: 'shop #5',
-            description: 'online retailer #5',
-            amount: '-101',
-            differed: false,
-            date: '2016-11-01'
+            id: 2,
+            topic: 'Amazon web Services - Basics',
+            description: 'Discover what AWS can offer your business with a brief overview followed by a look at some common services to enhance your experience and utilization.',
+            speaker: 'David',
+            attendies: 100,
+            price: '10$',
+            date: '2017-09-28',
+            webinar: "Cisco WebEx"
+        }, {
+            id: 3,
+            topic: 'Exporing Dockers',
+            description: 'Explore the possibilities of what Docker is, how to best fit it into your DevOps operations, and some of the potential it offers for your development environment.',
+            speaker: 'Philip',
+            attendies: 150,
+            price: '100$',
+            date: '2017-11-12',
+            webinar: "ClickWebinar"
+        }, {
+            id: 4,
+            topic: 'Blockchain for Enterprise',
+            description: 'Blockchain for Enterprise, to help you learn from the best programmers and domain experts from all over the world.',
+            speaker: 'Madhukar',
+            attendies: 300,
+            price: 'Free',
+            date: '2017-10-17',
+            webinar: "GoToWebinar"
+        }, {
+            id: 5,
+            topic: 'IOT- Introduction and Use Case',
+            description: 'Intro to Agile, DevOps, DevOps culture and Practices.',
+            speaker: 'Vijay',
+            attendies: 100,
+            price: 'Free',
+            date: '2017-09-30',
+            webinar: "Cisco WebEx"
+        }, {
+            id: 6,
+            topic: 'Amazon Web Services - Security and Identity',
+            description: 'The AWS Essentials course is for those who are new to Amazon Web Services. You will be introduced to AWS, setting up your own AWS account, and an overview of all of the AWS services',
+            speaker: 'David',
+            attendies: 200,
+            price: 'Free',
+            date: '2017-10-10',
+            webinar: "GoToWebinar"
+        }, {
+            id: 7,
+            topic: 'Swift for IOS - Basics',
+            description: ' You’ll be taught the basics of navigating the X-Code environment working with Views and Controllers, and many of the nuisances that application developers face through actually building your first iOS mobile application.',
+            speaker: 'Jordan',
+            attendies: 300,
+            price: 'Free',
+            date: '2017-11-01',
+            webinar: "Cisco WebEx"
         }];
     }
 
